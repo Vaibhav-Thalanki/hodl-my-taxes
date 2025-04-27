5773acb4898ed1744bb1d2b88b009994b3493983
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2025-04-27 04:07:44 -0400

    config chaged for next js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    // skip ESLint during builds
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    // skip TypeScript errors during builds
+    typescript: {
+      ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
